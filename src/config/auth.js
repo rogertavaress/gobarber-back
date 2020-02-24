847d3d8f9eae64a7edf5081a1cdc988831c127c6
Rogerio Tavares <rogerioctf@gmail.com>
2020-02-24 20:27:15 -0300

    Variáveis ambiente

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-    secret: 'bong ogre cony bide pods icon',
+    secret: process.env.APP_SECRET,
     expiresIn: '7d',
 };
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-    dsn: 'https://cb0ef7012c9a4af2935dfd200e1916f0@sentry.io/2832864',
+    dsn: process.env.SENTRY_DSN,
 };
